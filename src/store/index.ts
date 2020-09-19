d46942d9532fcc7e775409d7c527e6808bd1d7e0
Tim <timirwing@gmail.com>
2020-09-19 21:08:56 +0300

    vuex-simple init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { createVuexStore, Module } from 'vuex-simple';
+
+import { Calendar } from "@/store/modules/Calendar";
+import { Todo } from "@/store/modules/Todo";
+import { Calculator } from "@/store/modules/Calculator";
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+class RootStore {
+  @Module()
+  public calendar = new Calendar()
+
+  @Module()
+  public todo = new Todo()
+
+  @Module()
+  public calculator = new Calculator()
+}
+
+export default createVuexStore(new RootStore(), {
+  strict: false,
 })

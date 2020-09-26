39e779adcbaa74d39810802c02e158b2af432c4a
Tim <timirwing@gmail.com>
2020-09-26 17:37:40 +0300

    add todo component and store logic

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,8 @@
 import { Component, Prop } from 'vue-property-decorator';
+import { useStore } from 'vuex-simple';
+import { RootStore } from '@/store';
+import { Todo } from '@/store/modules/Todos';
+
 import Card from '@/components/Card';
 import VueComponent from '../shims-vue';
 
@@ -6,13 +10,58 @@ import styles from './Todos.css?module';
 
 @Component
 export default class Todos extends VueComponent<{ date: Date }> {
+  store: RootStore = useStore(this.$store)
+
   @Prop({ default: () => new Date() })
   private date!: Date;
+
+  get todos() {
+    return this.store.todos.getTodos(this.date);
+  }
+
+  addTodo = this.store.todos.addTodo;
+
+  toggleTodo(todo: Todo): void {
+    this.store.todos.toggleTodo([this.date, todo]);
+  }
+
+  get todosMarkup() {
+    return (
+      <ul>
+        {
+          this.todos.map((todo: Todo) => (
+            <li>
+              <label>
+                <input type="checkbox" checked={todo.done} onChange={this.toggleTodo.bind(this, todo)}/>
+                <span>{todo.text}</span>
+              </label>
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
+
+  keydownHandler({ key, target }: KeyboardEvent): void {
+    if (key.toLowerCase() === 'enter') {
+      this.addTodo([
+        this.date,
+        { text: (target as HTMLInputElement).value },
+      ]);
+
+      // eslint-disable-next-line no-param-reassign
+      (target as HTMLInputElement).value = '';
+    }
+  }
 
   render() {
     return (
       <Card>
-        <p class={styles.todos__title}>События - { this.date.toLocaleDateString('ru-RU')}</p>
+        <p class={styles.todos__title}>События - {this.date.toLocaleDateString('ru-RU')}</p>
+
+        {this.todosMarkup}
+
+        <input type="text" placeholder="Текст" onKeydown={this.keydownHandler}/>
       </Card>
     );
   }
--- src/store/modules/Todos.ts
+++ src/store/modules/Todos.ts
@@ -1,12 +1,19 @@
-import { Getter, Mutation, State } from 'vuex-simple';
+import Vue from 'vue';
+import {
+  Action, Getter, Mutation, State,
+} from 'vuex-simple';
 
 function parseDate(date: Date): string {
   return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
 }
 
+function formatText(text: string): string {
+  return text.trim();
+}
+
 export interface Todo {
-  done: boolean;
   text: string;
+  done?: boolean;
 }
 
 export default class Todos {
@@ -15,7 +22,10 @@ export default class Todos {
 
   @Getter()
   public get hasTodo(): Function {
-    return (date: Date): boolean => parseDate(date) in this.todos;
+    return (date: Date): boolean => {
+      const day = parseDate(date);
+      return (day in this.todos) && !!this.todos[day].length;
+    };
   }
 
   @Getter()
@@ -27,14 +37,46 @@ export default class Todos {
     };
   }
 
-  @Mutation()
-  public addTodo(date: Date, todo: Todo): void {
-    const key = parseDate(date);
+  @Action()
+  public addTodo(payload: [Date, Todo]): void {
+    const [date, todo] = payload;
+    const formattedText = formatText(todo.text);
+    const dayTodos = this.todos[parseDate(date)];
 
-    if (this.hasTodo(date)) {
-      this.todos[key].push(todo);
-    } else {
-      this.todos[key] = [todo];
+    if (!formattedText || dayTodos?.some(({ text }) => text === formattedText)) return;
+
+    this.addTodoMutation(payload);
+  }
+
+  @Action()
+  public toggleTodo(payload: [Date, Todo]): void {
+    const [date, todo] = payload;
+    const formattedText = formatText(todo.text);
+    const dayTodos = this.todos[parseDate(date)];
+
+    if (!dayTodos?.some(({ text }) => text === formattedText)) return;
+
+    this.toggleTodoMutation(payload);
+  }
+
+  @Mutation()
+  private addTodoMutation([date, todo]: [Date, Todo]) {
+    const day = parseDate(date);
+
+    if (!this.hasTodo(date)) {
+      Vue.set(this.todos, day, []);
+    }
+
+    this.todos[day].push(todo);
+  }
+
+  @Mutation()
+  private toggleTodoMutation([date, todo]: [Date, Todo]) {
+    const dayTodos = this.todos[parseDate(date)];
+    const todoInStore: Todo | undefined = dayTodos?.find(({ text }: Todo) => text === todo.text);
+
+    if (todoInStore) {
+      Vue.set(todoInStore, 'done', !todoInStore.done);
     }
   }
 }
